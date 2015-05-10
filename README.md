70fb3d2bf536fed8471dde42e31b56d2208174b9
Douglas Land <dsl@looprock.com>
2015-05-10 00:44:42 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # dockingstation
-A docker 'proxy' service to enable/disable service discovery for docker containers
+A service to add  docker containers to consul for service discovery
+
+# Options
+
+-h/--help         -  print usage summary
+-i/--interactive  -  interactive mode: passthrough to docker CLI (not implemented yet)
+-d/--daemon       -  daemon mode: poll docker for new services and register them
+-e/--env          -  environment: this will add a tag inside consul for your environment
+--debug           -  enable debug output
+
+# General functionality
+This script will poll 'docker ps' for entries and add them to consul via API.
+
+# daemon mode
+This is currently the only available option. You can run this under something like supervisor as a 'background' process.
+
+# registering checks
+You can register checks via the consul KV store.
+checks should map to: http://localhost:8500/v1/kv/checks/[name]
+format maps to: https://www.consul.io/docs/agent/checks.html, minus the initial 'check' root
+Example:
+<pre>
+{
+  "id": "api",
+  "name": "HTTP API on port 5000",
+  "http": "http://localhost:5000/health",
+  "interval": "10s",
+  "timeout": "1s"
+}
+</pre>
+
+# Interactive mode
+I hope to add interactive mode to this script to use it as a 'passthrough' to the actual docker command so that you can realtime register/deregister services as they are manages from docker.
+
+# other stuff
+I should probably add a cron mode as well...
+
---
+++ dockingstation.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+import subprocess
+import shlex
+import re
+import sys
+import json
+import requests
+import getopt
+import time
+import base64
+
+def usage():
+  print sys.argv[0] + """ [options] [appname]
+  options:
+
+  -h/--help         -  print usage summary
+  -i/--interactive  -  interactive mode: passthrough to docker CLI (not implemented yet)
+  -d/--daemon       -  daemon mode: poll docker for new services and register them
+  -e/--env          -  environment
+  --debug           -  enable debug output
+"""
+
+env = False
+daemon = False
+interactive = False
+debug = False
+
+try:
+  opts, remainder = getopt.gnu_getopt(sys.argv[1:], "hide:", ["help", "interactive", "daemon", "env=", "debug"])
+except getopt.GetoptError:
+  usage()
+  sys.exit(2)
+for opt, arg in opts:
+  if opt in ("-h", "--help"):
+    usage()
+    sys.exit()
+  elif opt in ("-i", "--interactive"):
+    interactive = "True"
+  elif opt in ("-d", "--daemon"):
+    daemon = "True"
+  elif opt in ("-e", "--env"):
+    env = arg
+  elif opt in ("--debug"):
+    debug = "True"
+
+# generate nested python dictionaries, copied from here:
+# http://stackoverflow.com/questions/635483/what-is-the-best-way-to-implement-nested-dictionaries-in-python
+class AutoVivification(dict):
+  """Implementation of perl's autovivification feature."""
+  def __getitem__(self, item):
+    try:
+      return dict.__getitem__(self, item)
+    except KeyError:
+      value = self[item] = type(self)()
+      return value
+
+def comm(command_line):
+        process = subprocess.Popen(shlex.split(command_line), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        out, error = process.communicate()
+        return out
+
+if daemon:
+  while True:
+    j = AutoVivification()
+    # get output from docker ps
+    x = comm("docker ps").split("\n")
+    # remove the 'title' line
+    x.pop(0)
+    for i in x:
+      if i:
+        # split apart lines on 'more than 2 whitespaces'
+        y = re.split(r'\s{2,}', i)
+        # so for repo/foo:latest, turn repo and latest into tags for name foo
+        if env:
+          tags = [env,]
+        else:
+          tags = []
+        n1 = y[1].split("/")
+        n2 = y[1].split(":")
+        if n1[0]:
+          tags.append(n1[0])
+        if n2[1]:
+          tags.append(n2[1])
+        name = n1[1].split(":")[0]
+        # now look for checks in kv
+        # checks should map to: http://localhost:8500/v1/kv/checks/[name]
+        # format maps to: https://www.consul.io/docs/agent/checks.html, minus the initial 'check' root
+        # Example:
+        #{
+        #  "id": "api",
+        #  "name": "HTTP API on port 5000",
+        #  "http": "http://localhost:5000/health",
+        #  "interval": "10s",
+        #  "timeout": "1s"
+        #}
+        r = requests.get("http://localhost:8500/v1/kv/checks/%s" % name)
+        if r.content:
+          check = json.loads(base64.b64decode(json.loads(r.content)[0]['Value']))
+        else:
+          check = False
+        if debug:
+          print check
+        # look for forwarded ports and create a consul service entry for each of them
+        for p in y[5].split(","):
+          m = re.match("(\d+\.+)+(\d:)(\d+)->(\d+)\/(\w+)", p.strip())
+          if m != None:
+            port =  int(p.strip().split(":")[1].split("->")[0])
+            j['name'] = name
+            j['tags'] = tags
+            j['port'] = port
+            if check:
+              j['check'] = check
+            if debug:
+              print j
+            r = requests.post("http://127.0.0.1:8500/v1/agent/service/register", data=json.dumps(j))
+            if r.status_code != 200:
+              print "ERROR: request failed with status code %s" % str(r.status_code)
+              print r.content
+              sys.exit(1)
+    time.sleep(5)
+
+else:
+  print "ERROR: no valid arguments specified!"
+  usage()
+  sys.exit(1)
