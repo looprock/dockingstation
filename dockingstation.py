b5936bb1090cffa7ae97b5315b9ac3cc24ade2bf
Douglas Land <dsl@looprock.com>
2015-05-17 11:07:06 -0500

    add support for mapping multiple services, templatizing checks to map to docker host port

--- dockingstation.py
+++ dockingstation.py
@@ -8,6 +8,7 @@ import requests
 import getopt
 import time
 import base64
+from jinja2 import Environment
 
 def usage():
   print sys.argv[0] + """ [options] [appname]
@@ -90,60 +91,74 @@ def poll_docker():
           print "Tags:"
           print tags
         name = n1[1].split(":")[0]
-        # now look for checks in kv
-        # checks should map to: http://localhost:8500/v1/kv/checks/[name]
-        # format maps to: https://www.consul.io/docs/agent/checks.html, minus the initial 'check' root
-        # Example:
-        #{
-        #  "id": "api",
-        #  "name": "HTTP API on port 5000",
-        #  "http": "http://localhost:5000/health",
-        #  "interval": "10s",
-        #  "timeout": "1s"
-        #}
-        r = requests.get("http://localhost:8500/v1/kv/checks/%s" % name)
-        if r.content:
-          check = json.loads(base64.b64decode(json.loads(r.content)[0]['Value']))
-        else:
-          check = False
+	# pull a list of service port mappings
+        # we need this to support multiple ports inside a container
+        # if this doesn't exist, dockingstation will blow up and refues to register the service
         if debug:
-          print check
+           print "Trying: http://localhost:8500/v1/kv/service/%s/ports" % name
+        r = requests.get("http://localhost:8500/v1/kv/service/%s/ports" % name)
+        if r.content:
+          if debug:
+             print base64.b64decode(json.loads(r.content)[0]['Value'])
+          ports = json.loads(base64.b64decode(json.loads(r.content)[0]['Value']))
+        else:
+          print "ERROR: please populate ports for service %s since none will show up under dockingstation!"
         # look for forwarded ports and create a consul service entry for each of them
         for p in y[5].split(","):
           m = re.match("(\d+\.+)+(\d:)(\d+)->(\d+)\/(\w+)", p.strip())
           if m != None:
-            # putting the name here is bad, and if multiple ports are present this will overwrite the name
-            # I will fix this when it's not 1am
-            port =  int(p.strip().split(":")[1].split("->")[0])
-            j['name'] = name
-            j['tags'] = tags
-            j['port'] = port
-            if check:
-              j['check'] = check
-            if debug:
-              print j
-            r = requests.post("http://127.0.0.1:8500/v1/agent/service/register", data=json.dumps(j))
-            return r
+            port =  p.strip().split(":")[1].split("->")
+            dockerport = int(port[1].split("/")[0])
+            if str(dockerport) not in ports.keys():
+               print "ERROR: unable to find port %s in service map. Please provide port mappings. Ignoring service!" % str(dockerport)
+               return []
+            else:
+               hostport = int(port[0])
+               # now look for checks in kv
+               # checks should map to: http://localhost:8500/v1/kv/checks/[name]
+               # format maps to: https://www.consul.io/docs/agent/checks.html, minus the initial 'check' root
+               # get a list of check
+               # use jinja template to map to docker host port
+               chk = requests.get("http://localhost:8500/v1/kv/service/%s/check" % name)
+               if chk.content:
+                   j['check'] = Environment().from_string(base64.b64decode(json.loads(chk.content)[0]['Value'])).render(checkport=hostport)
+                   if debug:
+                       print "DEBUG: found check:"
+                       print j['check']
+               if debug:
+		   print "## PORT"
+		   print p
+		   print "Docker port: %s" % str(dockerport)
+		   print "Host port: %s" % str(hostport)
+               # We use the port kv map to create a unique name per port for the image
+               j['name'] = "%s-%s" % (name, ports[str(dockerport)])
+               j['tags'] = tags
+               j['port'] = hostport
+               if debug:
+                 print j
+               r = requests.post("http://127.0.0.1:8500/v1/agent/service/register", data=json.dumps(j))
+               return r
 
 if daemon:
     while True:
         r = poll_docker()
+        if r:
+            if r.status_code != 200:
+                print "ERROR: request failed with status code %s" % str(r.status_code)
+                print r.content
+            else:
+                if debug:
+                    print r.content
+        time.sleep(5)
+elif once:
+    r = poll_docker()
+    if r:
         if r.status_code != 200:
             print "ERROR: request failed with status code %s" % str(r.status_code)
             print r.content
         else:
             if debug:
                 print r.content
-        time.sleep(5)
-elif once:
-    r = poll_docker()
-    if r.status_code != 200:
-        print "ERROR: request failed with status code %s" % str(r.status_code)
-        print r.content
-        sys.exit(1)
-    else:
-        if debug:
-            print r.content
 else:
   print "ERROR: no valid arguments specified!"
   usage()
